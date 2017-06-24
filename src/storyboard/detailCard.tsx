4677a34fb16b8e9bbad31f324ccd3d187ebbd49c
Tamás Szabó <szabototo89@hotmail.com>
2017-06-24 19:55:25 +0200

    Add typed properties to detail card

--- src/storyboard/detailCard.tsx
+++ src/storyboard/detailCard.tsx
@@ -1,7 +1,14 @@
 import * as React from 'react';
+import Detail from "../models/detail";
 
-const DetailCard = () => {
-  return <div>DetailCard</div>;
+type Properties = {
+  detail: Detail;
+}
+
+const DetailCard = ({ detail }: Properties) => {
+  return <div className="detail-card">
+    {detail.content}
+  </div>;
 };
 
 export default DetailCard;
