ba59431414495abd0e8f6dfbd9ef4a30639529e0
Tamás Szabó <szabototo89@hotmail.com>
2017-06-24 21:59:17 +0200

    Fix type errors in the build

--- src/common/container.tsx
+++ src/common/container.tsx
@@ -1,14 +1,14 @@
 import * as React from 'react';
 import {ReactElement} from "react";
 
-const getContainers = (children: Array<ReactElement> = []) => {
-  const isComponentContainer = (child: ReactElement) => child.type === Container;
+const getContainers = (children: Array<ReactElement<any>> = []) => {
+  const isComponentContainer = (child: ReactElement<any>) => child.type === Container;
   const isSubComponent = (child: ReactElement<{ isSubComponent: boolean }>) => child.props && child.props.isSubComponent;
 
   return children.filter(child => isComponentContainer(child) && isSubComponent(child));
 };
 
-const Container = ({ name, modifier, children }) => {
+const Container = ({ name, modifier, children }: any) => {
   const nameWithModifier = !!modifier ? `${name}--${modifier}` : '';
 
   return <div className={`${name} ${nameWithModifier}`}>
