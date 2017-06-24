f132f7c9bdafba6c393c955f61783c003cd4bdc9
Tamás Szabó <szabototo89@hotmail.com>
2017-06-24 17:19:02 +0200

    Remove initial file contents

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,1 @@
 import './app.css';
-
-function initializeApplication() {
-  const message: string = "Hello World!";
-  console.log(message);
-}
-
-initializeApplication();
-
-document.body.innerHTML = `
-  <h1>Hello World!</h1>
-`.trim(); 
