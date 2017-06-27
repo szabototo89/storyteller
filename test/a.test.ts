38730db2eb7fd3278868b096db2315c9b0dfd3b0
Tamás Szabó <szabototo89@hotmail.com>
2017-06-27 23:38:38 +0200

    Setup karma

---
+++ test/a.test.ts
@@ -0,0 +1,18 @@
+import Application from '../src/application';
+import { expect } from 'chai';
+
+describe('hello', () => {
+  it('world 1', () => {
+    debugger
+    expect(Application).does.exist;
+  });
+
+  it('world 2', () => {
+    expect(Application).does.exist;
+  });
+
+  it('world 3', () => {
+    expect(Application).does.exist;
+  });
+});
+    
