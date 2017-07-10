d84dfeb351c1e2fc3a47d374e3f9bba2e61d305d
Tamás Szabó <szabototo89@hotmail.com>
2017-07-10 21:18:12 +0200

    Add more tests to dashboard component.

--- test/storyboard/dashboard.test.tsx
+++ test/storyboard/dashboard.test.tsx
@@ -3,10 +3,14 @@ import { expect } from "chai";
 import { mount, shallow } from "enzyme";
 
 import { Dashboard } from "storyboard/dashboard";
-import { EpicBuilder, anEpic } from "test/builders/epicBuilder";
+import { EpicBuilder, anEpic, anEpicWithId } from "test/builders/epicBuilder";
 import { TaskGroup } from "models/taskGroup";
 import { aTask } from "test/builders/taskBuilder";
 import { aTaskGroup } from "test/builders/taskGroupBuilder";
+import { build } from "test/utils/builder";
+import { equalsText } from "test/utils/enzymeHelpers";
+import { Epic } from "models/epic";
+import { aStoryWithId } from "test/builders/storyBuilder";
 
 describe("Dashboard component", () => {
   it("should have a .dashboard element", () => {
@@ -19,17 +23,72 @@ describe("Dashboard component", () => {
 
   describe("epics", () => {
     it("are got as property", () => {
-      const aSimpleEpic = anEpic().build();
+      const epics = build([anEpic()]);
 
-      const passingEpics = () => shallow(<Dashboard epics={[aSimpleEpic]} />);
+      const passingEpics = () => shallow(<Dashboard epics={epics} />);
 
       expect(passingEpics).does.not.throw();
     });
 
     it("is optional", () => {
-      const passingEpics = () => shallow(<Dashboard epics={undefined} />);
+      const epics = undefined;
+
+      const passingEpics = () => shallow(<Dashboard epics={epics} />);
 
       expect(passingEpics).does.not.throw();
+    });
+
+    it("shows every epic content", () => {
+      const epics = build([
+        anEpicWithId().withContent("test epic"),
+        anEpicWithId().withContent("test epic 2")
+      ]);
+
+      const component = mount(<Dashboard epics={epics} />);
+
+      const containsEveryEpicContent = ["test epic", "test epic 2"].every(
+        equalsText(component)
+      );
+      expect(containsEveryEpicContent).is.true;
+    });
+
+    it("shows every story content", () => {
+      const epics = build([
+        anEpicWithId().withStories(
+          aStoryWithId().withContent("test content"),
+          aStoryWithId().withContent("test content 2")
+        ),
+        anEpicWithId().withStories(aStoryWithId().withContent("test content 3"))
+      ]);
+
+      const component = mount(<Dashboard epics={epics} />);
+
+      const containsEveryStoryContent = [
+        "test content",
+        "test content 2",
+        "test content 3"
+      ].every(equalsText(component));
+      expect(containsEveryStoryContent).is.true;
+    });
+
+    it("shows every task content", () => {
+      const epics = build([
+        anEpicWithId().withStories(
+          aStoryWithId()
+            .withContent("test content")
+            .withTasks(
+              aTask().withContent("test task 1"),
+              aTask().withContent("test task 2")
+            )
+        )
+      ]);
+
+      const component = mount(<Dashboard epics={epics} />);
+
+      const containsEveryTaskContent = ["test task 1", "test task 2"].every(
+        equalsText(component)
+      );
+      expect(containsEveryTaskContent).is.true;
     });
   });
 });
