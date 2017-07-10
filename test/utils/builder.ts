fff76c6a786ee708793fd52495595d05d65c83d4
Tamás Szabó <szabototo89@hotmail.com>
2017-07-10 23:47:01 +0200

    Move build function to testBuilder utils

--- test/utils/builder.ts
+++
@@ -1,5 +0,0 @@
-import { TestBuilder } from "test/utils/testBuilder";
-
-export const build = <Value>(builders: Array<TestBuilder<Value>>): Array<Value> => {
-  return builders.map(builder => builder.build());
-};
