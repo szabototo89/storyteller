68f1eaf06dfba580702500c1a4d4a7d20752400b
Tamás Szabó <szabototo89@hotmail.com>
2017-07-15 12:06:18 +0200

    Add className property to textbox

--- src/common/textBox.tsx
+++ src/common/textBox.tsx
@@ -1,13 +1,19 @@
 import * as React from "react";
 
 type Properties = {
+  className?: string;
   onChange?: (value: string) => void;
   value?: string;
 };
 
-const TextBox = ({ onChange, value }: Properties) => {
+export function TextBox({ onChange, value, className }: Properties) {
   const handleChange = (ev: any) => onChange && onChange(ev.target.value);
-  return <input type="text" value={value} onChange={handleChange} />;
+  return (
+    <input
+      className={className}
+      type="text"
+      value={value}
+      onChange={handleChange}
+    />
+  );
 };
-
-export default TextBox;
